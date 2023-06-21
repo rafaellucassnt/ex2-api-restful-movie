2a003fda3b152e41aaac14fae1f3046c7a02dab2
Rafael Lucas <rafael.lucas.4400@gmail.com>
2023-06-21 17:23:39 -0300

    [feat] implementação da rota de documentação

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 const moviesRouter = require('./routes/movies');
-
-app.get('/', (req, res) => {
-  res.send('Exercício 2 - Montagem da API RESTful com BD - Rafael Lucas ')
-})
+const swaggerSpec = require('../swagger');
 
 app.use(express.json());
-app.use('/api/filmes', moviesRouter);
+
+app.get('/', (req, res) => {
+  res.send('Exercício 2 - Montagem da API RESTful com BD - Rafael Lucas - <br><br><a href="/api-docs">Documentação API</a>')
+})
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use('/filmes', moviesRouter);
 
 const port = 3000;
 app.listen(port, () => {
