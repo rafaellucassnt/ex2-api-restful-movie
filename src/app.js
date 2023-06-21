768e005ad89351a4f597fad5a8cdfccd9b62c4a5
Rafael Lucas <rafael.lucas.4400@gmail.com>
2023-06-21 15:14:50 -0300

    [feat] Implementação da rota filmes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const moviesRouter = require('./routes/movies');
 
-app.get('/', (req, res) => {
-  res.send('Exercício 2 - Montagem da API RESTful com BD - Rafael Lucas dos Santos')
-})
+app.use(express.json());
+app.use('/api/filmes', moviesRouter);
 
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Servidor iniciado na porta ${port}`)
-})
+  console.log(`Servidor iniciado!`);
+});
